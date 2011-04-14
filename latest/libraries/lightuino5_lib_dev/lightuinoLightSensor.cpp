f2bd89608cd07eaf380440ebb3b2535b0eefea4c
G.Andrew.Stone <G.Andrew.Stone@2c98f414-479c-11de-b25d-9f9778916a3c>
2011-04-14 19:32:44 +0000

    reorganization

---
+++ latest/libraries/lightuino5_lib_dev/lightuinoLightSensor.cpp
@@ -0,0 +1,8 @@
+#include "lightuino.h"
+#include "lightuinoLightSensor.h"
+#include "wiring.h"
+
+LightSensor::LightSensor(unsigned char panaPin):pin(panaPin) {}    
+
+int LightSensor::read(void) { return analogRead(pin); };    
+
